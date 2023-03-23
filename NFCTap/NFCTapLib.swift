402efe5e0b948605dbdda12deb2a53e4f1224b69
arathi <arathi.c@softmantics.com>
2023-03-23 21:10:47 +0530

    Add NFC Tap

---
+++ NFCTap/NFCTapLib.swift
@@ -0,0 +1,17 @@
+//
+//  NFCTapLib.swift
+//  NFCTap 
+//
+//  Created by Ananthu KS on 21/03/2023.
+//  Copyright © 2023 STMicroelectronics. All rights reserved.
+//
+
+import UIKit
+import CoreNFC
+
+@objc public class NFCTapLib: NSObject {
+
+    public var mSession:NFCTagReaderSession!
+    
+
+}
---
+++ ST25NFCFramework/ST25NFCFramework.h
@@ -0,0 +1,18 @@
+//
+//  ST25NFCFramework.h
+//  ST25NFCFramework
+//
+//  Created by Ananthu KS on 21/03/2023.
+//  Copyright © 2023 STMicroelectronics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//! Project version number for ST25NFCFramework.
+FOUNDATION_EXPORT double ST25NFCFrameworkVersionNumber;
+
+//! Project version string for ST25NFCFramework.
+FOUNDATION_EXPORT const unsigned char ST25NFCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ST25NFCFramework/PublicHeader.h>
+
+
